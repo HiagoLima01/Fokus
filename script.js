d59bc680daf9f33e424f7f4adfb9457eca658a36
HiagoLima01 <hiago1234657@gmail.com>
2024-04-11 16:30:14 -0300

    Finalizando parte do cronômetro

--- script.js
+++ script.js
@@ -1,75 +1,108 @@
+//Selecionando Elementos do HTML
+
 const html = document.querySelector('html');
+
+//Selecionando botões do modo do app
 const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
 const botoes = document.querySelectorAll('.app__card-button')
 
+//Selecionando botões para play no timer e musica
 const iniciarBtn = document.getElementById('start-pause');
+const iniciarBtnTxt = document.querySelector('#start-pause span');
+const playPause = document.getElementById('start-pause');
 
+//Selecionando objetos para mudança de tela
 const tituloApp = document.querySelector('.app__title');
-
 const imagem = document.querySelector('.app__image');
+const tempoNaTela = document.getElementById('timer');
 
-const timer = document.getElementById('timer');
-
+//Selecionando botão para play na música
 const musicaFocoInpt = document.getElementById('alternar-musica');
+
+//Instânciando objetos da classe Audio para colocar música no site 
 const musica = new Audio('sons/luna-rise-part-one.mp3');
 const somInicio = new Audio('sons/play.wav')
 const somPause = new Audio('sons/pause.mp3')
 const somFim = new Audio('sons/beep.mp3')
 musica.loop = true;
 
-const playPause = document.getElementById('start-pause')
+//Iniciando variaveis de tempo e de timer para os modos
+let tempoDecorrido = 1500
+let intervaloId = null
 
-let tempoFoco = 1500;
-let tempoCurto = 300;
-let tempoLongo = 900;
 
+//Funções
 function iniciarOuPausar(){
+    /*
+    Função para iniciar e pausar música
+
+    Se o timer estiver tocando seu estado é true e o algoritimo irá entrar no if que ira pausar a música e mudar o texto do botão para 'começar' e zerar o timer
+    
+    Caso o timer não esteja tocando ela vai iniciar e junto vai definir um intervalo de tempo
+    
+    */
     if(intervaloId){
         somPause.play()
+        iniciarBtn.textContent = "Começar"
         zerar()
         return
     }
     somInicio.play()
     intervaloId = setInterval(contagemRegressiva, 1000)
+    iniciarBtn.textContent = "Pausar"
+}
+
+function mostrarTempo(){
+    /*
+    Função para mostrar na página o tempo corrido
+    Cria um objeto Date para fazer a formatação da data de segundos para minutos
+    */
+    const tempo = new Date(tempoDecorrido * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br',{'minute':'2-digit', 'second': '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
 }
 
 function zerar() {
+    /*
+    Retira o timer e define a variavel dele para NULL
+    */
     clearInterval(intervaloId) 
     intervaloId = null
 }
 
-
 const contagemRegressiva = () => {
-    if(tempoFoco <= 0){
-        somFim.play()
-        zerar()
+    /*
+    Função que vai tocar com o intervalo de tempo
+    Entra na condicional se o tempo chegar a zero, quando entrar na condicional toca que o timer chegou ao fim e zera o timer
+    Enquanto o Timer não chegou ao fim ele retira 1 a cada um segundo ele tira 1 no valor do tempo
+    */
+
+    if(tempoDecorrido <= 0){
+        somFim.play()   
         alert('Tempo finalizado!')
+        zerar()
         return
     }
-    tempoFoco -= 1
-    console.log('Temporizador: ' + tempoFoco)
+    tempoDecorrido -= 1
+    mostrarTempo()
 
 }
 
-
-let intervaloId = null
-
-
-
-playPause.addEventListener('click', iniciarOuPausar)
-
-musicaFocoInpt.addEventListener('change', () => {
-    if (musica.paused){
-        musica.play()
-    }else{
-        musica.pause()
-    }
-})
-
 function alterarContexto(contexto){
+    /*
+    Função que vai alterar o estado do programa dependendo da opção que o usuário selecionar
+    Define o valor do atributo 'data-contexto' no html para o tipo do botão
+    Muda a imagem da página
+
+    Switch Case para alterar título do programa dependendo do botão clicado
+    */
+    mostrarTempo()
     botoes.forEach(function (contexto) {
+        `
+        Remove as decorações de selecionado de todos botões
+        `
         contexto.classList.remove('active')
     })
 
@@ -91,18 +124,34 @@ function alterarContexto(contexto){
     }
 }
 
+//Listeners 
+
+playPause.addEventListener('click', iniciarOuPausar)
+
+musicaFocoInpt.addEventListener('change', () => {
+    if (musica.paused){
+        musica.play()
+    }else{
+        musica.pause()
+    }
+})
 
 focoBtn.addEventListener('click', () =>{
+    tempoDecorrido = 1500
     alterarContexto('foco')
     focoBtn.classList.add('active')
 } )
 
 curtoBtn.addEventListener('click', () =>{
+    tempoDecorrido = 300
     alterarContexto('descanso-curto')
     curtoBtn.classList.add('active')
 })
 
 longoBtn.addEventListener('click', () =>{
+    tempoDecorrido = 900
     alterarContexto('descanso-longo')
     longoBtn.classList.add('active')
 })
+
+mostrarTempo()
