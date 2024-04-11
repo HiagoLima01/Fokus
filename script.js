09684d459372fdd1613d9a01fd6373d9064c61ff
HiagoLima01 <hiago1234657@gmail.com>
2024-04-11 11:31:00 -0300

    Aplicando funcionalidades ao projeto

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const html = document.querySelector('html');
 const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
+const botoes = document.querySelectorAll('.app__card-button')
 
 const iniciarBtn = document.getElementById('start-pause');
 
@@ -11,14 +12,70 @@ const imagem = document.querySelector('.app__image');
 
 const timer = document.getElementById('timer');
 
-const tempoFoco = 1500;
-const tempoCurto = 300;
-const tempoLongo = 900;
+const musicaFocoInpt = document.getElementById('alternar-musica');
+const musica = new Audio('sons/luna-rise-part-one.mp3');
+const somInicio = new Audio('sons/play.wav')
+const somPause = new Audio('sons/pause.mp3')
+const somFim = new Audio('sons/beep.mp3')
+musica.loop = true;
 
+const playPause = document.getElementById('start-pause')
+
+let tempoFoco = 1500;
+let tempoCurto = 300;
+let tempoLongo = 900;
+
+function iniciarOuPausar(){
+    if(intervaloId){
+        somPause.play()
+        zerar()
+        return
+    }
+    somInicio.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervaloId) 
+    intervaloId = null
+}
+
+
+const contagemRegressiva = () => {
+    if(tempoFoco <= 0){
+        somFim.play()
+        zerar()
+        alert('Tempo finalizado!')
+        return
+    }
+    tempoFoco -= 1
+    console.log('Temporizador: ' + tempoFoco)
+
+}
+
+
+let intervaloId = null
+
+
+
+playPause.addEventListener('click', iniciarOuPausar)
+
+musicaFocoInpt.addEventListener('change', () => {
+    if (musica.paused){
+        musica.play()
+    }else{
+        musica.pause()
+    }
+})
 
 function alterarContexto(contexto){
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active')
+    })
+
     html.setAttribute('data-contexto', contexto)
     imagem.setAttribute('src', `/imagens/${contexto}.png`)
+
     switch(contexto){
         case 'foco':
             tituloApp.innerHTML = `Otimize sua produtividade,<br><strong class="app__title-strong">mergulhe no que importa.</strong>`
@@ -34,8 +91,18 @@ function alterarContexto(contexto){
     }
 }
 
-focoBtn.addEventListener('click', () =>{alterarContexto('foco')} )
 
-curtoBtn.addEventListener('click', () =>{alterarContexto('descanso-curto')})
+focoBtn.addEventListener('click', () =>{
+    alterarContexto('foco')
+    focoBtn.classList.add('active')
+} )
 
-longoBtn.addEventListener('click', () =>{alterarContexto('descanso-longo')})
+curtoBtn.addEventListener('click', () =>{
+    alterarContexto('descanso-curto')
+    curtoBtn.classList.add('active')
+})
+
+longoBtn.addEventListener('click', () =>{
+    alterarContexto('descanso-longo')
+    longoBtn.classList.add('active')
+})
