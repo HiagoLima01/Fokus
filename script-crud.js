6bbd5abcb5d7ad0c241601aeea48c1f7c019ae03
HiagoLima01 <hiago1234657@gmail.com>
2024-04-16 16:03:22 -0300

    aplicando botão de enviar

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,11 @@
-const adicionarTarefaBtn = document.querySelector('.app__button--add-task')
-const adicionarTarefaForm = document.querySelector('.app__form-add-task')
+const adicionarTarefaBtn = document.querySelector('.app__button--add-task');
+const adicionarTarefaForm = document.querySelector('.app__form-add-task');
+const areaDeTexto = document.querySelector('.app__form-textarea');
 
 adicionarTarefaBtn.addEventListener('click', ()=>{
-    formAdicionarTarefa.classList.toggle('hidden');
+    adicionarTarefaForm.classList.toggle('hidden');
+})
+adicionarTarefaForm.addEventListener('submit', (evento)=>{
+    evento.preventDefault();
+    const texto = areaDeTexto.value
 })
